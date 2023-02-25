2302a6d6b2ca487a25a190841c71799815942a4a
SAKE CHAITANYA <61873017+sakechaithanya@users.noreply.github.com>
2023-02-25 12:32:01 +0530

    Update assignment4.py

--- assignment4.py
+++ assignment4.py
@@ -1,4 +1,14 @@
-#Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument
+#1q Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument
 x=int(input('enter the number :'))
 a = lambda i : i+25
 print(a(x))
+
+# 2q Write a Python program to triple all numbers of a given list of integers. Use Python map.
+lis=[]
+n=int(input('enter the number:'))
+for i in range(1,n):
+    lis.append(i)
+def triple(y):
+    return 3*y
+result=list(map(triple,lis))
+print(result)
