e05cd6c38cfd86328e56013e0ab691d6cf4cee49
SAKE CHAITANYA <61873017+sakechaithanya@users.noreply.github.com>
2023-02-25 13:13:15 +0530

    Update assignment4.py

--- assignment4.py
+++ assignment4.py
@@ -6,19 +6,22 @@ print(a(x))
 # 2q Write a Python program to triple all numbers of a given list of integers. Use Python map.
 lis=[]
 n=int(input('enter the number:'))
-for i in range(1,n):
-    lis.append(i)
+for i in range(0,n):
+    value1=int(input('enter the value:'))
+    lis.append(value1)
 def triple(y):
     return 3*y
 result=list(map(triple,lis))
 print(result)
 
 #3q Write a Python program to square the elements of a list using map() function.
+
 def square(z):
     return z*z
 lis1=[]
 num=int(input('enter the number:'))
-for i in range(1,num):
-    lis1.append(i)
+for i in range(0,num):
+    value=int(input('enter the value:'))
+    lis1.append(value)
 output=list(map(square,lis1))    
 print(output)
